e82486bddd79682f3418c254f5dd881700738110
Sayan Snigdha Pal <sayanpalb@gmail.com>
2023-08-19 03:01:51 +0530

    navbar done

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from './Billing';
+import Business from './Business';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import Footer from './Footer';
+import Testimonials from './Testimonials';
+import Stats from './Stats';
+import Hero from './Hero';
+import Navbar from './Navbar';
+
+export {
+    Navbar,
+    Hero,
+    Stats,
+    Testimonials,
+    Footer,
+    CTA,
+    Clients,
+    CardDeal,
+    Business,
+    Billing,
+};
